ac96a0b6ad16cc6598b17960689e9566e7790f97
Dibyam <dibyam1jalan@gmail.com>
2024-05-01 11:15:43 +0530

    Snake movement

--- src/Board/BoardV2.jsx
+++ src/Board/BoardV2.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { ROW, COL, Direction } from "../lib/constants";
 
 import "./Board.css";
-import { randomIntFromInterval } from "../lib/utils";
+import { randomIntFromInterval, useInterval } from "../lib/utils";
 
 class LinkedListNode {
   constructor(row, col) {
@@ -13,10 +13,11 @@ class LinkedListNode {
 }
 
 class LinkedList {
-  constructor(row, col, direction) {
+  constructor(row, col, direction, length) {
     this.head = new LinkedListNode(row, col);
     this.tail = this.head;
     this.direction = direction;
+    this.length = length;
   }
 }
 
@@ -44,19 +45,22 @@ function Board2() {
     let cell = board[row][col];
     cells.set(cell, cells[cell] || 0 + 1);
     const length = randomIntFromInterval(1, 5);
-    const direction = getRandomDirection(Direction.RIGHT);
+    const direction = getRandomDirection();
     const snake = new LinkedList(row, col, direction);
     let current = snake.head;
+    snake.length = length;
     for (let i = 1; i < length; i++) {
       if (row < ROW - 1) {
         row++;
         current.next = new LinkedListNode(row, col);
+        snake.tail = current.next;
         current = current.next;
         cell = board[row][col];
         cells.set(cell, cells[cell] || 0 + 1);
       } else {
         col++;
         current.next = new LinkedListNode(row, col);
+        snake.tail = current.next;
         current = current.next;
         cell = board[row][col];
         cells.set(cell, cells[cell] || 0 + 1);
@@ -65,6 +69,111 @@ function Board2() {
     setSnakeCells(cells);
     setSnakes([...snakes, snake]);
   };
+
+  const moveSnake = () => {
+    const newSnakes = [];
+    const newSnakeCells = snakeCells;
+    snakes.forEach((snake) => {
+      const { head, tail, direction, length } = snake;
+      console.log(tail);
+      let newHead = getCoordsInDirection(head, direction);
+      if (isOutOfBounds(newHead, board)) {
+        const tempDirection = getOppositeDirection(direction);
+        snake.direction = tempDirection;
+        let prev = null;
+        let current = head;
+        let next = null;
+        while (current != null) {
+          next = current.next;
+          current.next = prev;
+          prev = current;
+          current = next;
+        }
+        snake.head = prev;
+        snake.tail = head;
+        newHead = getCoordsInDirection(head, tempDirection);
+      }
+      const newHeadCell = board[newHead.row][newHead.col];
+
+      const tailCell = board[tail.row][tail.col];
+      newSnakeCells.set(newHeadCell, newSnakeCells[newHeadCell] || 0 + 1);
+      console.log(newSnakeCells);
+      const lastTail = newSnakeCells[tailCell];
+      if (lastTail > 1) {
+        newSnakeCells.set(tailCell, lastTail - 1);
+      } else {
+        newSnakeCells.delete(tailCell);
+      }
+      console.log(newSnakeCells, tailCell);
+      snake.head = newHead;
+      newHead.next = head;
+      let current = snake.head;
+      for (let i = 1; i < length; i++) {
+        current = current.next;
+      }
+      snake.tail = current;
+      newSnakes.push(snake);
+    });
+    setSnakes(newSnakes);
+    setSnakeCells(newSnakeCells);
+  };
+
+  const handleDirectionChange = (coords, board) => {
+    const { row, col } = coords;
+    // console.log(`Game over! Snake ran into (${row}, ${col})`);
+    if (row < 0) {
+      if (col === 0) {
+        return Direction.RIGHT;
+      } else if (col === board[0].length - 1) {
+        return Direction.LEFT;
+      } else {
+        const randomDirection =
+          Math.random() < 0.5 ? Direction.LEFT : Direction.RIGHT;
+        return randomDirection;
+      }
+    } else if (row >= board.length) {
+      if (col === 0) {
+        return Direction.RIGHT;
+      } else if (col === board[0].length - 1) {
+        return Direction.LEFT;
+      } else {
+        const randomDirection =
+          Math.random() < 0.5 ? Direction.LEFT : Direction.RIGHT;
+        return randomDirection;
+      }
+    } else if (col < 0) {
+      if (row === 0) {
+        return Direction.DOWN;
+      } else if (row === board.length - 1) {
+        return Direction.UP;
+      } else {
+        const randomDirection =
+          Math.random() < 0.5 ? Direction.UP : Direction.DOWN;
+        return randomDirection;
+      }
+    } else if (col >= board[0].length) {
+      if (row === 0) {
+        return Direction.DOWN;
+      } else if (row === board.length - 1) {
+        return Direction.UP;
+      } else {
+        const randomDirection =
+          Math.random() < 0.5 ? Direction.UP : Direction.DOWN;
+        return randomDirection;
+      }
+    }
+  };
+
+  const isOutOfBounds = (coords, board) => {
+    const { row, col } = coords;
+    if (row < 0 || col < 0) return true;
+    if (row >= board.length || col >= board[0].length) return true;
+    return false;
+  };
+
+  useInterval(() => {
+    if (gameState === "running") moveSnake();
+  }, 2000);
 
   const handleMouseHover = (row, col) => {
     if (gameState === "over") return;
@@ -100,8 +209,8 @@ function Board2() {
       // console.log(key);
       if (key === " ") {
         if (gameState === "over" || gameState === "Start") {
-          setGameState("running");
-          setSnakeCells(() => new Map());
+          setSnakes([]);
+          setSnakeCells();
           generateSnake();
           setScore(0);
           setLevel(1);
@@ -111,7 +220,9 @@ function Board2() {
             cell: board[9][0],
           });
           setDiamond(randomIntFromInterval(1, ROW * COL));
+          setGameState("running");
         } else {
+          setSnakes([]);
           setSnakeCells(() => new Map());
           setGameState("over");
         }
@@ -169,6 +280,7 @@ function Board2() {
           </div>
         ))}
       </div>
+      <div>Press Space to Pause/Restart</div>
     </>
   );
 }
@@ -186,16 +298,10 @@ const createBoard = (BOARD_ROW, BOARD_COL) => {
   return board;
 };
 
-const getRandomDirection = (currentDirection) => {
+const getRandomDirection = () => {
   const directions = Object.values(Direction);
-  const oppositeDirection = getOppositeDirection(currentDirection);
-  const validDirections = directions.filter((dir) => dir !== oppositeDirection);
-  const changeDirection = Math.random() < 0.3;
-  if (changeDirection) {
-    const randomIndex = Math.floor(Math.random() * validDirections.length);
-    return validDirections[randomIndex];
-  }
-  return currentDirection;
+  const randomIndex = Math.floor(Math.random() * directions.length);
+  return directions[randomIndex];
 };
 
 const getOppositeDirection = (direction) => {
@@ -203,6 +309,36 @@ const getOppositeDirection = (direction) => {
   if (direction === Direction.RIGHT) return Direction.LEFT;
   if (direction === Direction.DOWN) return Direction.UP;
   if (direction === Direction.LEFT) return Direction.RIGHT;
+};
+
+const getCoordsInDirection = (coords, direction) => {
+  // const direction = getRandomDirection(currDirection);
+  // setDirection(direction);
+
+  if (direction === Direction.UP) {
+    return {
+      row: coords.row - 1,
+      col: coords.col,
+    };
+  }
+  if (direction === Direction.RIGHT) {
+    return {
+      row: coords.row,
+      col: coords.col + 1,
+    };
+  }
+  if (direction === Direction.DOWN) {
+    return {
+      row: coords.row + 1,
+      col: coords.col,
+    };
+  }
+  if (direction === Direction.LEFT) {
+    return {
+      row: coords.row,
+      col: coords.col - 1,
+    };
+  }
 };
 
 export default Board2;
